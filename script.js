b37293d57497e4800c03591a75a3e5286d840662
Jason Chen <jasonchen6464@gmail.com>
2023-05-17 17:15:22 -0700

    added backend code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let latitude;
-let longitude;
+let latitude = 0;
+let longitude = 0;
 
 window.onload = function() {
     const date = new Date();
@@ -11,19 +11,44 @@ window.onload = function() {
     } else {
       console.log("Geolocation is not available in your browser.");
     }
-}
     
-function success(position){
-	latitude = position.coords.latitude;
-	longitude = position.coords.longitude;
-    //alert(latitude + " " + longitude)
-}
+    function success(position){
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        //alert(latitude + " " + longitude)
+    }
 
-const btn = document.getElementById("getWeatherBtn");
-btn.addEventListener("click", () => {
-    let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
-    let forecastElements = document.getElementsByClassName("forecast");
-    for (let i = 0; i < forecast.length; i++) {
-        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
-      }
-});
+    const btn = document.getElementById("getWeatherBtn");
+    btn.addEventListener("click", () => {
+
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`);
+        xhr.send();
+
+        xhr.onload = function() {
+            let body = JSON.parse(xhr.responseText)
+            var temperature = body.temperature
+            var weatherStatus = body.weatherStatus
+            document.getElementById('temperature').innerHTML = `Temperature: ${temperature}°F`
+            document.getElementById('weatherStatus').innerHTML = `Weather Status: ${weatherStatus}`
+        }
+
+        
+
+
+        const xhr2 = new XMLHttpRequest(); // Defines the XMLHttp object
+        xhr2.open("GET", `http://localhost:3000/5day/${latitude}/${longitude}`); // opens a get request to the website
+        xhr2.send(); // sends the request
+
+        xhr2.onload = function(){ // Once we get a response
+            const body = JSON.parse(xhr2.responseText);
+            let forecast = body; // Parse the forecast from the response, we now have a list of 5 day/temperature pairs
+            // forecast = [{Monday: 52}, {Tuesday: 53}, {Wednesday: 54}, {Thursday: 55}, {Friday: 56}}]
+            let forecastElements = document.getElementsByClassName("forecast"); // Setting forecastElements to an array of divs with the class 'forecast' (5 in this case): [first div, second div, third div]
+            for (let i = 0; i < forecast.length; i++){
+            forecastElements[i].innerHTML = forecast[i].dayName + ": " + forecast[i].temp + "\u00B0F";
+            }
+        }
+    })
+}
